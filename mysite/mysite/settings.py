28e34c29dd2f23ad6e0a5425c3aaa6731de966e8
GOSBaag <galvanobaag@live.nl>
2023-10-25 15:42:50 +0200

    update admin voetbalspelers

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w4x870poc3!(w%dz5$85jd%&61%8ut_*t%yk7*s)8$ixo0tob-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
