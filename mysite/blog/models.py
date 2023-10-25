d7f3af589ed336df7b0dfba9a594b5216ae9eb8e
GOSBaag <galvanobaag@live.nl>
2023-10-25 15:50:23 +0200

    auteur update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,9 +10,9 @@ class Post(models.Model):
    published_date = models.DateTimeField(blank=True, null=True)
 
 class Voetbalspelers(models.Model):   
-   naam_van_de_voetballer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+   naam_van_de_voetballer = models.TextField()
    actuele_voetbalclub = models.CharField(max_length=200)
-   auteur = models.TextField()
+   auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
    created_date = models.DateTimeField(default=timezone.now)
    published_date = models.DateTimeField(blank=True, null=True)
 
