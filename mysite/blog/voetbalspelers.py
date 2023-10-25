e14844445efa93f58d89a0019a609828cc21a0d1
GOSBaag <galvanobaag@live.nl>
2023-10-25 17:52:41 +0200

    voetbalpage

--- mysite/blog/voetbalspelers.py
+++ mysite/blog/voetbalspelers.py
@@ -3,14 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):   
-   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-   title = models.CharField(max_length=200)
-   text = models.TextField()
-   created_date = models.DateTimeField(default=timezone.now)
-   published_date = models.DateTimeField(blank=True, null=True)
-
-
 class Voetbalspelers(models.Model):   
    auteur = models.CharField(max_length=200)
    voetbalspeler = models.CharField(max_length=200)
@@ -24,4 +16,3 @@ class Voetbalspelers(models.Model):
 
    def __str__(self):
        return self.title
-
